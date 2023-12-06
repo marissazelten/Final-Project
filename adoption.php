28930fa3c12f8805e04b471701dfddbb4bfd72b7
marissazelten <144714187+marissazelten@users.noreply.github.com>
2023-12-06 11:35:29 -0500

    Update adoption.php

--- adoption.php
+++ adoption.php
@@ -5,14 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Woofs and Wags</title>
     <!-- page style -->
-   <style>
+    <style>
         body {
             font-family: "Arial", sans-serif;
             margin: 20px;
+              display: block;
+  margin-left: auto;
+  margin-right: auto;
         }
+        .center {
+  margin: auto;
+  width: 50%;
+  border: 3px solid green;
+  padding: 10px;
+}
 
         h1 {
             color: #333;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
         }
 
         .dogs-container {
@@ -26,73 +38,76 @@
             padding: 15px;
             background-color: #f9f9f9;
             max-width: 300px;
+              display: block;
+  margin-left: auto;
+  margin-right: auto;
         }
 
         .dogs-item img {
             max-width: 100%;
             height: auto;
             margin-bottom: 10px;
+              display: block;
+  margin-left: auto;
+  margin-right: auto;
         }
     </style>
 </head>
 <body>
   
 <?php
-
-//establish connection info
 $servername = "localhost";
 $username = "uuu9s7lqeekc7";
-$password = "dogDatabase!";
+$password = "dogdatabase1";
 $dbname = "dbklmqdle5ki76";
 
-//create connection
 $connection = new mysqli($servername, $username, $password);
 
-//check to see if connection was established 
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
 
-//Header title 
 echo "<h1>Find Your Furry Friend</h1>";
 
-
-//select the database
 $connection->select_db($dbname);
 
-//run sql query 
-$sql = "SELECT * FROM Dogs"; 
+$sql = "SELECT * FROM Dogs WHERE Status = 'available'"; 
 $results = $connection->query($sql); 
 
-
 if ($results->num_rows > 0) {
-    $i = 0;
     while ($row = $results->fetch_assoc()) {
-        $itemId = $i;
-?>
-<?php
-        for ($x = 0; $x <= breeds.length(); $x++) {
-            if(($row['Breed'] LIKE '%breeds[x]%') and ($row['Status'] == '%available%')) {
-                <div class='dogs'>
-                    <p><?php echo '<img src="' . $row['Image'] . '" alt="Image" style="max-width: 100px; height: auto;">'; ?></p>
-                    <p>Name: <?php echo $row['Name']; ?></p>
-                    <p>Price: <?php echo '$' . $row['Breed']; ?></p>
-                    <p>Description: <?php echo $row['Description']; ?></p>
-                    <p>Availability: <?php echo $row['Status']; ?></p>
+        $matchedBreeds = array("Terrier", "Retriever", "Lab");
 
-
-                </div>
-            }
+        if (in_array($row['Breed'], $matchedBreeds)) {
+            echo "<div class='dogs-item'>";
+            echo "<p><img src='{$row['Image']}' alt='Image' style='max-width: 100px; height: auto;'></p>";
+            echo "<p>Name: {$row['Name']}</p>";
+            echo "<p>Breed: {$row['Breed']}</p>";
+            echo "<p>Description: {$row['Description']}</p>";
+            echo "<p>Availability: {$row['Status']}</p>";
+            echo "</div>";
         }
-?>      
- 
-<?php
-        $i++;
     }
 } else {
     echo "No results";
 }
 
-//close connection
 $connection->close();
 ?>
+
+<form action="/action_page.php">
+  <label for="fname">Dog name:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value=""><br><br>
+    <label for="fname">Address:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+    <label for="fname">Why you want to adopt this dog:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
