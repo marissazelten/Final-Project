8f268b60378e6d715987385db6ef1225f44b7eaf
esasonov1 <118038727+esasonov1@users.noreply.github.com>
2023-12-08 11:45:03 -0500

    Update adopt.php

--- adopt.php
+++ adopt.php
@@ -175,39 +175,44 @@
             $username = "uuu9s7lqeekc7";
             $password = "dogdatabase1";
             $dbname = "dbklmqdle5ki76";
-
-            $connection = new mysqli($servername, $username, $password);
-
+            
+            $connection = new mysqli($servername, $username, $password, $dbname);
+            
             if ($connection->connect_error) {
                 die("Connection failed: " . $connection->connect_error);
             }
-
+            
             echo "<h1>Find Your Furry Friend</h1>";
-
-            $connection->select_db($dbname);
-
+            
             $sql = "SELECT * FROM Dogs WHERE Status = 'available'"; 
             $results = $connection->query($sql); 
-
+            $matchedBreeds = array("Boston Terrier", "Golden Retriever", "Black Lab");
+            
             if ($results->num_rows > 0) {
                 while ($row = $results->fetch_assoc()) {
-
-                    if (in_array($row['Breed'], $matchedBreeds)) {
-                        echo "<div class='dogs-item'>";
-                        echo "<p><img src='{$row['Image']}' alt='Image' style='max-width: 100px; height: auto;'></p>";
-                        echo "<p>Name: {$row['Name']}</p>";
-                        echo "<p>Breed: {$row['Breed']}</p>";
-                        echo "<p>Description: {$row['Description']}</p>";
-                        echo "<p>Availability: {$row['Status']}</p>";
-                        echo "</div>";
+                    $ourCurrentBreed = strtolower($row['Breed']);
+                 
+                    for ($i = 0; $i < count($matchedBreeds); $i++) {
+                        $current_breed = strtolower($matchedBreeds[$i]);
+            
+            
+                if (strpos($current_breed, $ourCurrentBreed) !== false) {
+                            echo "<div class='dogs-item'>";
+                            echo "<p><img src='{$row['Image']}' alt='Image' style='max-width: 100%; height: auto;'></p>";
+                            echo "<p>Name: {$row['Name']}</p>";
+                            echo "<p>Breed: {$row['Breed']}</p>";
+                            echo "<p>Description: {$row['Description']}</p>";
+                            echo "<p>Availability: {$row['Status']}</p>";
+                            echo "</div>";
+                        }
                     }
                 }
             } else {
                 echo "No results";
             }
-
+            
             $connection->close();
-            ?>
+        ?>
 
             <form action="/action_page.php">
             <label for="fname">Dog name:</label><br>
