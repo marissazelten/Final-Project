faef3f45fc1b0319ea5071b394737b8d246caa7d
marissazelten <144714187+marissazelten@users.noreply.github.com>
2023-12-10 13:53:00 -0500

    Update availability.php

--- availability.php
+++ availability.php
@@ -1,16 +1,8 @@
 <?php
-//actually db info
 $servername = "localhost";
 $username = "uuu9s7lqeekc7";
 $password = "dogdatabase1";
 $dbname = "dbklmqdle5ki76";
-
-
-// //Emmas test db info
-// $servername = "localhost";
-// $username = "uctaqyp6whqgb";
-// $password = "H$44mblcz#$1";
-// $dbname = "dbb9amnxg8mjho";
 
 $connection = new mysqli($servername, $username, $password, $dbname);
 
@@ -19,9 +11,9 @@ if ($connection->connect_error) {
 }
 
 $dogNames = json_decode($_POST['dogNames']);
-
-
+$availableDogs = array();
 $matchedBreeds = array();
+$chosenDog = array();
 
 $sql = "SELECT * FROM Dogs WHERE Status = 'available'";
 $results = $connection->query($sql);
@@ -30,22 +22,24 @@ if ($results->num_rows > 0) {
     while ($row = $results->fetch_assoc()) {
         $ourCurrentBreed = strtolower($row['Breed']);
 
-        for ($i = 0; $i < count($dogNames); $i++) {
-            $current_breed = strtolower($dogNames[$i]);
-        // for ($i = 0; $i < count($matchedBreeds); $i++) {
-        //     $current_breed = strtolower($matchedBreeds[$i]);
+        // Check if the dog has already been chosen
+        if (!in_array($row['Name'], $chosenDog, FALSE)) {
+            for ($i = 0; $i < count($dogNames); $i++) {
+                $current_breed = strtolower($dogNames[$i]);
 
-            if (strpos($current_breed, $ourCurrentBreed) !== false) {
-                $dogInfo = array(
-                    'Image' => $row['Image'],
-                    'Name' => $row['Name'],
-                    'Breed' => $row['Breed'],
-                    'Description' => $row['Description'],
-                    'Status' => $row['Status']
-                );
-
-                $availableDogs[] = $dogInfo;
-                $matchedBreeds[] = $row['Breed'];
+                if (strpos($current_breed, $ourCurrentBreed) !== false) {
+                    $dogInfo = array(
+                        'Image' => $row['Image'],
+                        'Name' => $row['Name'],
+                        'Breed' => $row['Breed'],
+                        'Description' => $row['Description'],
+                        'Status' => $row['Status']
+                    );
+                    $availableDogs[] = $dogInfo;
+                    $matchedBreeds[] = $row['Breed'];
+                    $chosenDog[] = $row['Name'];
+                    break; // Break out of the loop once a match is found
+                }
             }
         }
     }
@@ -58,3 +52,5 @@ echo json_encode($availableDogs);
 
 $connection->close();
 ?>
+
+
