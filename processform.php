af0a31d7ae84f617ff0b5e32cbf7706ab9686ef5
marissazelten <144714187+marissazelten@users.noreply.github.com>
2023-12-10 13:53:58 -0500

    Update processform.php

--- processform.php
+++ processform.php
@@ -23,8 +23,11 @@ echo "<p>We're excited to hear about your new adventure!</p>";
 
 //if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $Name = $_GET['dogname'];
+    $email_address = $_GET['email_address'];
+    $fname = $_GET['firstname'];
 
-    $sql = "UPDATE Dogs SET Status='Unavailable' WHERE Name='$Name'";
+
+    $sql = "UPDATE Dogs SET Status='unavailable' WHERE Name='$Name'";
     if ($connection->query($sql) === TRUE) {
         echo "<p>Thanks for adopting: " . $Name . "</p>";
     } else {
@@ -33,6 +36,12 @@ echo "<p>We're excited to hear about your new adventure!</p>";
 //}
 
 $connection->close();
+$email = " Hello ". $fname . ", \n Thank you for adopting "  .$Name  . "\n Thanks for making a difference in your new furry friend's life by giving them a furrever home! \n \n";
+
+ $email = wordwrap($email,70);
+ $headers = "From: alibbe01@tufts.edu";
+ $to = $_GET['email_address']; //change this part to retrieve email from your form
+ $success = mail($to, "Your Mutts and Meows Order", $email, $headers);
 
 ?>
 
